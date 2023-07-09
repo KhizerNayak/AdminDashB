639baa45320fd248dca80acaa3941cadc96fb573
khizer <khizer.naik@gmail.com>
2023-07-09 06:02:57 +0530

    here this is an updated file for admin dashboard panel

---
+++ order.js
@@ -0,0 +1,27 @@
+const Orders = [
+    {
+        productName: 'Product 1',
+        productNumber: '1',
+        PaymentStatus : 'Due',
+        status:'Pending'
+    },
+    {
+        productName: 'Product 2',
+        productNumber: '2',
+        PaymentStatus : 'Refunded',
+        status:'Declined'
+    },
+    {
+        productName: 'Product 3',
+        productNumber: '3',
+        PaymentStatus : 'Paid',
+        status:'Completed'
+    },
+    {
+        productName: 'Product 4',
+        productNumber: '4',
+        PaymentStatus : 'Paid',
+        status:'Delayed'
+    }
+
+]
